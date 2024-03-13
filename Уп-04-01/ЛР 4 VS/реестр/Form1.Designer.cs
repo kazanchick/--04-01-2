f17009e5ec401598b4f6870c8f947c9e108a22a2
IODINOPOL <148897062+NikitaSumin06@users.noreply.github.com>
2024-03-13 10:26:35 +0300

    Add files via upload

---
+++ Уп-04-01/ЛР 4 VS/реестр/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace реестр
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 87);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 31);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "первая тема";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 142);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(147, 30);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "вторая тема";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(176, 123);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(272, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Привет привет привет привет привет привет привет";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(551, 317);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Уп-04-01/ЛР 4 VS/реестр/Form1.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace реестр
+{
+    public partial class Form1 : Form
+    {
+        private int _clr = -1;
+        private string _str;
+        const string userRoot = "HKEY_CURRENT_USER";
+        const string subkey = "RegistrySetValueExample";
+        const string keyName = userRoot + "\\" + subkey;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if ((int)Registry.GetValue(keyName, "clr", "-1") == 0)
+                Collor(Color.Yellow, Color.Gray);
+            if ((int)Registry.GetValue(keyName, "clr", "-1") == 1)
+                Collor(Color.LightBlue, Color.White);
+            if ((int)Registry.GetValue(keyName, "clr", "-1") == 2)
+                Collor(Color.Pink, Color.White);
+
+        }
+       
+        private void Collor(Color buttonc, Color backgroundc)
+        {
+            button1.BackColor = buttonc;
+            button2.BackColor = buttonc;
+            
+            this.BackColor = backgroundc;
+            label1.BackColor = buttonc;
+        }
+        
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Collor(Color.Yellow, Color.Gray);
+            _clr = 0;
+            Registry.SetValue(keyName, "clr", 0);
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            Collor(Color.LightBlue, Color.White);
+            _clr = 1;
+            Registry.SetValue(keyName, "clr", 1);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            Collor(Color.Pink, Color.White);
+            _clr = 1;
+            Registry.SetValue(keyName, "clr", 2);
+        }
+    }
+}
+
